0544e61232c4bd6ee58ca446ec3d0e15992dba47
Zrax <zrax0111@gmail.com>
2010-04-12 05:56:59 +0000

    I've put this off for too long

--- Python/examples/prp-listobjects.py
+++ Python/examples/prp-listobjects.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# This file is part of HSPlasma.
+#
+# HSPlasma is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# HSPlasma is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with HSPlasma.  If not, see <http://www.gnu.org/licenses/>.
+
 # This script reads a prp file (passed as first argument) and prints the names of all scene objects.
 # Alternatively, you can specifiy the type to be listed as second argument.
 # by Diafero
