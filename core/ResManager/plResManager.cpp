H-uru/libhsplasma

commit 37ea10c8aa90e628f610510b778ad3d7fb06a59f

Make the progress callback more useful

core/ResManager/plResManager.cpp
@@ -154,7 +154,7 @@ plPageInfo* plResManager::ReadPage(const char* filename, bool stub) {
     pages.push_back(page);
     setVer(S->getVer(), true);
     S->seek(page->getIndexStart());
-    totalKeys = ReadKeyring(S, loc);
+    ReadKeyring(S, loc);
     if (!packed) {
         S->close();
         delete S;
@@ -164,7 +164,6 @@ plPageInfo* plResManager::ReadPage(const char* filename, bool stub) {
         if (!S->open(file, fmRead))
             throw hsFileReadException(__FILE__, __LINE__, file.cstr());
     }
-    readKeys = 0;
     mustStub = stub;
     page->setNumObjects(ReadObjects(S, loc));
     mustStub = false;
@@ -298,20 +297,6 @@ plAgeInfo* plResManager::ReadAge(const char* filename, bool readPages) {
 
         hsFileStream* S = NULL;
         std::vector<plPageInfo*> agepages;
-        totalKeys = 0;
-        readKeys = 0;
-
-        for (size_t i=0; i<age->getNumPages(); i++) {
-            if (hsFileStream::FileExists(path + age->getPageFilename(i, ageVer))) {
-                S = new hsFileStream();
-                if (!S->open(path + age->getPageFilename(i, ageVer), fmRead)) {
-                    throw hsFileReadException(__FILE__, __LINE__, filename);
-                }
-                totalKeys += ReadPage(S, agepages);
-                S->close();
-                delete S;
-            }
-        }
 
         for (size_t i=0; i<age->getNumCommonPages(ageVer); i++) {
             if (hsFileStream::FileExists(path + age->getCommonPageFilename(i, ageVer))) {
@@ -319,7 +304,19 @@ plAgeInfo* plResManager::ReadAge(const char* filename, bool readPages) {
                 if (!S->open(path + age->getCommonPageFilename(i, ageVer), fmRead)) {
                     throw hsFileReadException(__FILE__, __LINE__, filename);
                 }
-                totalKeys += ReadPage(S, agepages);
+                ReadPage(S, agepages);
+                S->close();
+                delete S;
+            }
+        }
+
+        for (size_t i=0; i<age->getNumPages(); i++) {
+            if (hsFileStream::FileExists(path + age->getPageFilename(i, ageVer))) {
+                S = new hsFileStream();
+                if (!S->open(path + age->getPageFilename(i, ageVer), fmRead)) {
+                    throw hsFileReadException(__FILE__, __LINE__, filename);
+                }
+                ReadPage(S, agepages);
                 S->close();
                 delete S;
             }
@@ -486,8 +483,21 @@ unsigned int plResManager::ReadObjects(hsStream* S, const plLocation& loc) {
     std::vector<short> types = keys.getTypes(loc);
     unsigned int nRead = 0;
 
-    if (progressFunc != NULL)
-        progressFunc(0.0f);
+    size_t totalKeys = 0, processedKeys = 0;
+
+    for (unsigned int i=0; i<types.size(); i++) {
+        std::vector<plKey> kList = keys.getKeys(loc, types[i]);
+        totalKeys += kList.size();
+    }
+
+    plPageInfo *page = 0;
+    if (progressFunc != NULL) {
+        for (size_t i = 0; i < pages.size(); ++i) {
+            if (pages[i]->getLocation() == loc)
+                page = pages[i];
+        }
+        progressFunc(page, 0, totalKeys);
+    }
 
     for (unsigned int i=0; i<types.size(); i++) {
         std::vector<plKey> kList = keys.getKeys(loc, types[i]);
@@ -496,6 +506,8 @@ unsigned int plResManager::ReadObjects(hsStream* S, const plLocation& loc) {
                        types[i], pdUnifiedTypeMap::ClassName(types[i]));
 #endif
         for (unsigned int j=0; j<kList.size(); j++) {
+            ++processedKeys;
+
             if (kList[j]->getFileOff() <= 0)
                 continue;
 #ifdef RMTRACE
@@ -520,7 +532,6 @@ unsigned int plResManager::ReadObjects(hsStream* S, const plLocation& loc) {
                 }
                 if (kList[j]->getObj() != NULL) {
                     nRead++;
-                    readKeys++;
                     if (!subStream->eof()) {
                         plDebug::Warning("[%04hX:%s] Size-Read difference: %d bytes left after reading",
                             kList[j]->getType(), kList[j]->getName().cstr(),
@@ -548,7 +559,7 @@ unsigned int plResManager::ReadObjects(hsStream* S, const plLocation& loc) {
         }
 
         if (progressFunc != NULL)
-            progressFunc((float)(readKeys)/(totalKeys));
+            progressFunc(page, processedKeys, totalKeys);
     }
 
     return nRead;
@@ -730,8 +741,8 @@ void plResManager::ChangeLocation(plLocation from, plLocation to) {
     keys.ChangeLocation(from, to);
 }
 
-ProgressCallback plResManager::SetProgressFunc(ProgressCallback newFunc) {
-    ProgressCallback old = progressFunc;
+LoadProgressCallback plResManager::SetProgressFunc(LoadProgressCallback newFunc) {
+    LoadProgressCallback old = progressFunc;
     progressFunc = newFunc;
     return old;
 }

core/ResManager/plResManager.h
@@ -29,8 +29,8 @@
 #include "plAgeInfo.h"
 #include "Sys/hsThread.h"
 
-/** Callback to indicate the progress of the current operation, range [0,1] */
-typedef std::function<void (float progress)> ProgressCallback;
+/** Callback to indicate the progress of the current load operation */
+typedef std::function<void (plPageInfo *page, size_t curObj, size_t maxObjs)> LoadProgressCallback;
 
 /** Callback to be called before page is unloaded */
 typedef std::function<void (const plLocation& loc)> PageUnloadCallback;
@@ -58,9 +58,8 @@ protected:
     plKeyCollector keys;
     std::vector<plPageInfo*> pages;
     std::vector<plAgeInfo*> ages;
-    ProgressCallback progressFunc;
+    LoadProgressCallback progressFunc;
     PageUnloadCallback pageUnloadFunc;
-    unsigned int totalKeys, readKeys;
     bool mustStub;
 
 private:
@@ -80,10 +79,9 @@ public:
      * \sa setVer(), getVer()
      */
     plResManager(PlasmaVer pv = PlasmaVer::pvUnknown)
-        : fPlasmaVer(PlasmaVer::pvUnknown), totalKeys(0), readKeys(0), mustStub(false)
+        : fPlasmaVer(PlasmaVer::pvUnknown), mustStub(false)
     {
         setVer(pv);
-        progressFunc = ProgressCallback();
     }
 
     /**
@@ -397,7 +395,7 @@ public:
     /**
      * This function is currently unsupported.
      */
-    ProgressCallback SetProgressFunc(ProgressCallback newFunc);
+    LoadProgressCallback SetProgressFunc(LoadProgressCallback newFunc);
 
     PageUnloadCallback SetPageUnloadFunc(PageUnloadCallback newFunc);
 

commit 5fe895095ba9b4acdbc934583246821da84d9a5e

Fix debug build broken by 37ea10c

core/ResManager/plResManager.cpp
@@ -340,13 +340,6 @@ plAgeInfo* plResManager::ReadAge(const char* filename, bool readPages) {
             S->close();
             delete S;
         }
-
-#ifdef DEBUG
-        if (totalKeys != readKeys) {
-            plDebug::Error("* Keyring count %d but we could only read %d",
-                           totalKeys, readKeys);
-        }
-#endif
     }
 
     ages.push_back(age);