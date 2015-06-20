H-uru/libhsplasma

commit 5d4496e5fb6ff091ed4a6c2057b56fdcc2d5726b

Don't modify the vertex buffer while encoding it to a stream

core/PRP/Geometry/plVertCoder.cpp
@@ -79,7 +79,7 @@ void plVertCoder::IDecode(hsStream* S, unsigned char*& dest, unsigned char forma
     }
 }
 
-void plVertCoder::IDecodeByte(hsStream* S, int chan, unsigned char*& dest) {
+void plVertCoder::IDecodeByte(hsStream* S, int chan, unsigned char* dest) {
     if (fColors[chan].fCount == 0) {
         unsigned short count = S->readShort();
         if (count & 0x8000) {
@@ -92,10 +92,9 @@ void plVertCoder::IDecodeByte(hsStream* S, int chan, unsigned char*& dest) {
         fColors[chan].fCount = count;
     }
     if (fColors[chan].fSame)
-        *dest = fColors[chan].fVal;
+        dest[chan] = fColors[chan].fVal;
     else
-        *dest = S->readByte();
-    dest += sizeof(unsigned char);
+        dest[chan] = S->readByte();
     fColors[chan].fCount--;
 }
 
@@ -130,13 +129,11 @@ void plVertCoder::IDecodeNormal(hsStream* S, unsigned char*& dest) {
 }
 
 void plVertCoder::IDecodeColor(hsStream* S, unsigned char*& dest) {
-    unsigned int* col = (unsigned int*)dest;
     IDecodeByte(S, 0, dest);
-    IDecodeByte(S, 1, dest);
-    IDecodeByte(S, 2, dest);
     IDecodeByte(S, 3, dest);
-
-    *col = ((*col & 0x000000FF) << 16 | (*col & 0x00FF0000) >> 16 | (*col & 0xFF00FF00));
+    IDecodeByte(S, 2, dest);
+    IDecodeByte(S, 1, dest);
+    dest += sizeof(unsigned int);
 }
 
 void plVertCoder::IEncode(hsStream* S, unsigned int vertsLeft,
@@ -175,20 +172,19 @@ void plVertCoder::IEncode(hsStream* S, unsigned int vertsLeft,
 }
 
 void plVertCoder::IEncodeByte(hsStream* S, unsigned int vertsLeft, int chan,
-                              const unsigned char*& src, unsigned int stride) {
+                              const unsigned char* src, unsigned int stride) {
     if (fColors[chan].fCount == 0) {
-        ICountBytes(vertsLeft, src, stride, fColors[chan].fCount, fColors[chan].fSame);
+        ICountBytes(vertsLeft, src + chan, stride, fColors[chan].fCount, fColors[chan].fSame);
         unsigned short count = fColors[chan].fCount;
         if (fColors[chan].fSame)
             count |= 0x8000;
         S->writeShort(count);
         count &= 0x7FFF;
         if (fColors[chan].fSame)
-            S->writeByte(*src);
+            S->writeByte(src[chan]);
     }
     if (!fColors[chan].fSame)
-        S->writeByte(*src);
-    src += sizeof(unsigned char);
+        S->writeByte(src[chan]);
     fColors[chan].fCount--;
 }
 
@@ -234,12 +230,11 @@ void plVertCoder::IEncodeNormal(hsStream* S, const unsigned char*& src) {
 
 void plVertCoder::IEncodeColor(hsStream* S, unsigned int vertsLeft,
                                const unsigned char*& src, unsigned int stride) {
-    unsigned int* col = (unsigned int*)src;
-    *col = ((*col & 0x000000FF) << 16 | (*col & 0x00FF0000) >> 16 | (*col & 0xFF00FF00));
     IEncodeByte(S, vertsLeft, 0, src, stride);
-    IEncodeByte(S, vertsLeft, 1, src, stride);
-    IEncodeByte(S, vertsLeft, 2, src, stride);
     IEncodeByte(S, vertsLeft, 3, src, stride);
+    IEncodeByte(S, vertsLeft, 2, src, stride);
+    IEncodeByte(S, vertsLeft, 1, src, stride);
+    src += sizeof(unsigned int);
 }
 
 void plVertCoder::ICountBytes(unsigned int vertsLeft, const unsigned char* src,

core/PRP/Geometry/plVertCoder.h
@@ -50,7 +50,7 @@ private:
     } fColors[4];
 
     void IDecode(hsStream* S, unsigned char*& dest,unsigned char format);
-    void IDecodeByte(hsStream* S, int chan, unsigned char*& dest);
+    void IDecodeByte(hsStream* S, int chan, unsigned char* dest);
     void IDecodeFloat(hsStream* S, int field, int chan, unsigned char*& dest);
     void IDecodeNormal(hsStream* S, unsigned char*& dest);
     void IDecodeColor(hsStream* S, unsigned char*& dest);
@@ -58,7 +58,7 @@ private:
     void IEncode(hsStream* S, unsigned int vertsLeft, const unsigned char*& src,
                  unsigned int stride, unsigned char format);
     void IEncodeByte(hsStream* S, unsigned int vertsLeft, int chan,
-                     const unsigned char*& src, unsigned int stride);
+                     const unsigned char* src, unsigned int stride);
     void IEncodeFloat(hsStream* S, unsigned int vertsLeft, int field, int chan,
                       const unsigned char*& src, unsigned int stride);
     void IEncodeNormal(hsStream* S, const unsigned char*& src);

commit 7375dc32764675c84c5702b32351927921d70f22

Fix vertex color ordering issue
This fixes an issue with vertex color encoding/decoding introduced by
5d4496e. The colors were swapped in what appears to be an attempt to
replicate some unused debug bit shifting. Probably under the assumption
that input data is RGBA (Plasma expects BGRA). However input data is
assumed to be BGRA, as that is what hsColor32 uses under the hood.

core/PRP/Geometry/plVertCoder.cpp
@@ -130,9 +130,9 @@ void plVertCoder::IDecodeNormal(hsStream* S, unsigned char*& dest) {
 
 void plVertCoder::IDecodeColor(hsStream* S, unsigned char*& dest) {
     IDecodeByte(S, 0, dest);
-    IDecodeByte(S, 3, dest);
-    IDecodeByte(S, 2, dest);
     IDecodeByte(S, 1, dest);
+    IDecodeByte(S, 2, dest);
+    IDecodeByte(S, 3, dest);
     dest += sizeof(unsigned int);
 }
 
@@ -231,9 +231,9 @@ void plVertCoder::IEncodeNormal(hsStream* S, const unsigned char*& src) {
 void plVertCoder::IEncodeColor(hsStream* S, unsigned int vertsLeft,
                                const unsigned char*& src, unsigned int stride) {
     IEncodeByte(S, vertsLeft, 0, src, stride);
-    IEncodeByte(S, vertsLeft, 3, src, stride);
-    IEncodeByte(S, vertsLeft, 2, src, stride);
     IEncodeByte(S, vertsLeft, 1, src, stride);
+    IEncodeByte(S, vertsLeft, 2, src, stride);
+    IEncodeByte(S, vertsLeft, 3, src, stride);
     src += sizeof(unsigned int);
 }
 