286108d70ab7ea2b18ddb313969972b74c8ac4aa
Adam Johnson <AdamJohnso@gmail.com>
2020-08-27 00:19:53 -0400

    Make CMake config relocatable.
    While this should be functionally the same as before, it utlizes
    built-in CMake functionality so tools that depend on kludges like string
    replacement can more easily match what we're doing.

--- core/HSPlasmaConfig.cmake
+++
@@ -1,3 +0,0 @@
-get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/HSPlasma-targets.cmake)
-get_filename_component(HSPlasma_INCLUDE_DIRS "${SELF_DIR}/../../../include/HSPlasma" ABSOLUTE)
