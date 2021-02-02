6d2fe6999165e27f842799de4e07e86701ba9a63
Adam Johnson <AdamJohnso@gmail.com>
2021-02-02 11:17:54 -0500

    Fix PyTypeObject initializer for Python 3.9.

--- Python/PyPlasma.h
+++ Python/PyPlasma.h
@@ -592,9 +592,14 @@ template <> inline size_t pyPlasma_get(PyObject* value) { return (size_t)(unsign
 #endif
 
 // tp_print moved to the end, and two new vectorcall fields inserted in Python 3.8...
+// tp_print removed entirely from Python 3.9...
 #if (PY_MAJOR_VERSION >= 4) || ((PY_MAJOR_VERSION == 3) && (PY_MINOR_VERSION >= 8))
     #define _TP_PRINT_OR_VECTORCALL_OFFSET 0
+#if (PY_MAJOR_VERSION == 3) && (PY_MINOR_VERSION == 8)
     #define _TP_VECTORCALL_PRINT nullptr, nullptr,
+#else
+    #define _TP_VECTORCALL_PRINT nullptr,
+#endif
 #else
     #define _TP_PRINT_OR_VECTORCALL_OFFSET nullptr
     #define _TP_VECTORCALL_PRINT
