bf26756777e7e6bb6f58dd7e95dd972d001e9d72
Adam Johnson <AdamJohnso@gmail.com>
2021-06-07 14:19:01 -0400

    Change `pfSizedStream::seek` to do a straight assignment.

--- core/Stream/pfSizedStream.cpp
+++ core/Stream/pfSizedStream.cpp
@@ -41,7 +41,7 @@ void pfSizedStream::seek(uint32_t pos)
     }
 
     fBase->seek(fBegin + pos);
-    fPos = pos > fLength ? fLength : pos;
+    fPos = pos;
 }
 
 void pfSizedStream::skip(int32_t count)
